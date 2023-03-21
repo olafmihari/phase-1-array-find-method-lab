21c0d95dae8994b391914b93612d28df7cb5764c
Olaf Mihari <olafnjenga@gmail.com>
2023-03-21 18:07:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winRecord = record.find((item) => item.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
